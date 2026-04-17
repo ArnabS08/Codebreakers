bffc3333db0143f7572bd681cafd692f152546eb
Arnab Shrestha <arnab.shrestha08@gmail.com>
2026-04-17 01:53:56 -0500

    Caesar Cipher for txt

--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
-with open("Message.txt", "r", encoding="utf-8") as infile,\
-    open("MessageCopy.txt", "w", encoding="utf-8") as outfile:
-        for line in infile:
-            print(line.strip())
-            outfile.write(line)
-            outfile.write("This is a new line.\n")
+def caesar_cipher(file, shift):
+      with open(f"{file}.txt", "r", encoding="utf-8") as infile,\
+        open(f"Caesar_Ciphered_{file}.txt", "w", encoding="utf-8") as outfile:
+            for line in infile:
+                ciphered_line = ""
+                for char in line:
+                    if char.isalpha():
+                        char_code = ord(char)
+                        shifted_code = char_code + shift
+                        if char.islower():   
+                            if shifted_code > ord('z'):
+                                shifted_code -= 26
+                        elif char.isupper():
+                            if shifted_code > ord('Z'):
+                                shifted_code -= 26
+                        ciphered_line += chr(shifted_code)
+                    else:
+                        ciphered_line += char
+                print(ciphered_line.strip())
+                outfile.write(ciphered_line)
+
+caesar_cipher("Message", 3)
